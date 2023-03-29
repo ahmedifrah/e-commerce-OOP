7c682ac7f916562e6be3a3fcded99b1bdbcdb973
ahmedifrah <ahmed.ifrah.ibrahim@gmail.com>
2023-03-29 11:35:53 +0500

    add logic to Cart functions and add validation

--- Cart.php
+++ Cart.php
@@ -1,25 +1,44 @@
 <?php
-
+require_once 'Product.php';
 class Cart
 {
-    public function add()
-    {
+    // protected Product $product;
+    protected $cart = [];
 
+    // public function __construct(Product $product)
+    // {
+    //     $this->product = $product;
+    // }
+    public function add($product)
+    {
+        return $this->cart[] = $product;
     }
 
-    public function remove()
+    public function remove($product)
     {
+        if (($product = array_search($product, $this->cart)) !== false) {
+            unset($this->cart[$product]);
+        }
 
+        return $this->cart;
     }
 
     public function getTotalPrice()
     {
-
+        $price = 0;
+        foreach($this->cart as $_cart){
+            $price += $_cart->price;
+        }
+        return "Total Price of all the products in this cart is : \${$price}";
     }
 
     public function clearAll()
     {
-
+        if($this->cart === NULL){
+            return 'The cart is empty';
+        }else {
+            unset($this->cart);
+        }
     }
 
 }
