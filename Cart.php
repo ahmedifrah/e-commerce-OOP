dfb7d28cd7969548ff3e80eaa33262aaaf9a198d
ahmedifrah <ahmed.ifrah.ibrahim@gmail.com>
2023-03-29 10:27:37 +0500

    add function names

---
+++ Cart.php
@@ -0,0 +1,25 @@
+<?php
+
+class Cart
+{
+    public function add()
+    {
+
+    }
+
+    public function remove()
+    {
+
+    }
+
+    public function getTotalPrice()
+    {
+
+    }
+
+    public function clearAll()
+    {
+
+    }
+
+}
